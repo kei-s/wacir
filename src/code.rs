fdf83ca470e4bee55aaa55bebe92c976a0270173
kei-s <kei.shiratsuchi@gmail.com>
2020-05-10 01:28:19 +0900

    cargo fmt
    [日記]
    - (日記は後から)
    - 特になし
    - VSCode の設定で、Rust の時は format on save をオンにしたくらい

--- src/code.rs
+++ src/code.rs
@@ -4,171 +4,192 @@ use std::convert::TryInto;
 pub struct Instructions(pub Vec<u8>);
 
 pub fn concat_instructions(ins_vec: Vec<Instructions>) -> Instructions {
-    Instructions(ins_vec.into_iter().map(|ins| ins.0).collect::<Vec<Vec<u8>>>().concat())
+    Instructions(
+        ins_vec
+            .into_iter()
+            .map(|ins| ins.0)
+            .collect::<Vec<Vec<u8>>>()
+            .concat(),
+    )
 }
 
 impl std::fmt::Debug for Instructions {
-  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-    let mut out = String::new();
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let mut out = String::new();
 
-    let mut i = 0;
-    while i < self.0.len() {
-      let def = lookup(self.0[i]);
-      let (operands, read) = read_operands(&def, &self.0[i+1..]);
-      out.push_str(&format!("{:04} {}\n", i, self.fmt_instruction(def, operands)));
-      i += 1 + read;
+        let mut i = 0;
+        while i < self.0.len() {
+            let def = lookup(self.0[i]);
+            let (operands, read) = read_operands(&def, &self.0[i + 1..]);
+            out.push_str(&format!(
+                "{:04} {}\n",
+                i,
+                self.fmt_instruction(def, operands)
+            ));
+            i += 1 + read;
+        }
+
+        write!(f, "{}", out)
     }
-
-    write!(f, "{}", out)
-  }
 }
 
 impl Instructions {
-  fn print(&self) -> String {
-    let mut out = String::new();
+    fn print(&self) -> String {
+        let mut out = String::new();
 
-    let mut i = 0;
-    while i < self.0.len() {
-      let def = lookup(self.0[i]);
-      let (operands, read) = read_operands(&def, &self.0[i+1..]);
-      out.push_str(&format!("{:04} {}\n", i, self.fmt_instruction(def, operands)));
-      i += 1 + read;
-    }
-    out
-  }
-
-  fn fmt_instruction(&self, def: Definition, operands: Vec<u16>) -> String {
-    let operand_count = def.operand_width.len();
-
-    if operands.len() != operand_count {
-      return format!("ERROR: operand len {} does not match defined {}\n", operands.len(), operand_count);
+        let mut i = 0;
+        while i < self.0.len() {
+            let def = lookup(self.0[i]);
+            let (operands, read) = read_operands(&def, &self.0[i + 1..]);
+            out.push_str(&format!(
+                "{:04} {}\n",
+                i,
+                self.fmt_instruction(def, operands)
+            ));
+            i += 1 + read;
+        }
+        out
     }
 
-    match operand_count {
-      1 => format!("{} {}", def.name, operands[0]),
-      _ => format!("ERROR: unhundled operand_count for {}", def.name)
+    fn fmt_instruction(&self, def: Definition, operands: Vec<u16>) -> String {
+        let operand_count = def.operand_width.len();
+
+        if operands.len() != operand_count {
+            return format!(
+                "ERROR: operand len {} does not match defined {}\n",
+                operands.len(),
+                operand_count
+            );
+        }
+
+        match operand_count {
+            1 => format!("{} {}", def.name, operands[0]),
+            _ => format!("ERROR: unhundled operand_count for {}", def.name),
+        }
     }
-  }
 }
 
 type Opcode = u8;
 
 pub enum OpcodeType {
-  OpConstant
+    OpConstant,
 }
 
 impl OpcodeType {
-  pub fn from(op: Opcode) -> OpcodeType {
-    match op {
-      0 => OpcodeType::OpConstant,
-      _ => unreachable!("No such opcode {}", op)
+    pub fn from(op: Opcode) -> OpcodeType {
+        match op {
+            0 => OpcodeType::OpConstant,
+            _ => unreachable!("No such opcode {}", op),
+        }
     }
-  }
 
-  pub fn opcode(&self) -> Opcode {
-    match self {
-      OpcodeType::OpConstant => 0,
+    pub fn opcode(&self) -> Opcode {
+        match self {
+            OpcodeType::OpConstant => 0,
+        }
     }
-  }
 }
 
 pub struct Definition {
-  pub name: String,
-  pub operand_width: Vec<usize>
+    pub name: String,
+    pub operand_width: Vec<usize>,
 }
 
 pub fn lookup(op: Opcode) -> Definition {
-  match OpcodeType::from(op) {
-    OpcodeType::OpConstant => Definition{ name: "OpConstant".to_string(), operand_width: vec![2] }
-  }
+    match OpcodeType::from(op) {
+        OpcodeType::OpConstant => Definition {
+            name: "OpConstant".to_string(),
+            operand_width: vec![2],
+        },
+    }
 }
 
 pub fn make(op: Opcode, operand: u16) -> Instructions {
-  let def = lookup(op);
+    let def = lookup(op);
 
-  let mut instruction_len = 1;
-  for w in &def.operand_width {
-    instruction_len += w;
-  }
+    let mut instruction_len = 1;
+    for w in &def.operand_width {
+        instruction_len += w;
+    }
 
-  let mut instruction = Vec::with_capacity(instruction_len);
-  instruction.push(op);
+    let mut instruction = Vec::with_capacity(instruction_len);
+    instruction.push(op);
 
-  let bytes = operand.to_be_bytes();
-  instruction.extend_from_slice(&bytes);
+    let bytes = operand.to_be_bytes();
+    instruction.extend_from_slice(&bytes);
 
-  Instructions(instruction)
+    Instructions(instruction)
 }
 
 pub fn read_operands(def: &Definition, ins: &[u8]) -> (Vec<u16>, usize) {
-  let mut operands = Vec::with_capacity(def.operand_width.len());
-  let mut offset = 0;
+    let mut operands = Vec::with_capacity(def.operand_width.len());
+    let mut offset = 0;
 
-  for width in &def.operand_width {
-    match width {
-      2 => {
-        let i = ins[offset..offset+2].try_into().unwrap();
-        operands.push(u16::from_be_bytes(i));
-      }
-      _ => {}
+    for width in &def.operand_width {
+        match width {
+            2 => {
+                let i = ins[offset..offset + 2].try_into().unwrap();
+                operands.push(u16::from_be_bytes(i));
+            }
+            _ => {}
+        }
+        offset += width
     }
-    offset += width
-  }
 
-  (operands, offset)
+    (operands, offset)
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_make() {
-      let tests = vec![
-        ( OpcodeType::OpConstant.opcode(), 65534, Instructions(vec![0u8, 255u8, 254u8]))
-      ];
+    #[test]
+    fn test_make() {
+        let tests = vec![(
+            OpcodeType::OpConstant.opcode(),
+            65534,
+            Instructions(vec![0u8, 255u8, 254u8]),
+        )];
 
-      for (op, operand, expected) in tests {
-        let instruction = make(op, operand);
+        for (op, operand, expected) in tests {
+            let instruction = make(op, operand);
 
-        assert_eq!(instruction, expected);
-      }
-  }
+            assert_eq!(instruction, expected);
+        }
+    }
 
-  #[test]
-  fn test_instruction_string() {
-    let instructions = vec![
-      make(OpcodeType::OpConstant.opcode(), 1),
-      make(OpcodeType::OpConstant.opcode(), 2),
-      make(OpcodeType::OpConstant.opcode(), 65535)
-    ];
+    #[test]
+    fn test_instruction_string() {
+        let instructions = vec![
+            make(OpcodeType::OpConstant.opcode(), 1),
+            make(OpcodeType::OpConstant.opcode(), 2),
+            make(OpcodeType::OpConstant.opcode(), 65535),
+        ];
 
-    let expected = r"0000 OpConstant 1
+        let expected = r"0000 OpConstant 1
 0003 OpConstant 2
 0006 OpConstant 65535
 ";
-    let concatted = concat_instructions(instructions);
+        let concatted = concat_instructions(instructions);
 
-    assert_eq!(expected, concatted.print());
-  }
+        assert_eq!(expected, concatted.print());
+    }
 
-  #[test]
-  fn test_read_operands() {
-      let tests = vec![
-        (OpcodeType::OpConstant, 65535, 2)
-      ];
+    #[test]
+    fn test_read_operands() {
+        let tests = vec![(OpcodeType::OpConstant, 65535, 2)];
 
-      for (op, operand, bytes_read) in tests {
-        let instruction = make(op.opcode(), operand);
-        let def = lookup(op.opcode());
+        for (op, operand, bytes_read) in tests {
+            let instruction = make(op.opcode(), operand);
+            let def = lookup(op.opcode());
 
-        if let Some((_, ins)) = instruction.0.split_first() {
-          let (operands_read, n) = read_operands(&def, ins);
-          assert_eq!(bytes_read, n);
-          assert_eq!(operand, operands_read[0]);
-        } else {
-          unreachable!()
+            if let Some((_, ins)) = instruction.0.split_first() {
+                let (operands_read, n) = read_operands(&def, ins);
+                assert_eq!(bytes_read, n);
+                assert_eq!(operand, operands_read[0]);
+            } else {
+                unreachable!()
+            }
         }
-      }
-  }
+    }
 }
--- src/compiler.rs
+++ src/compiler.rs
@@ -1,102 +1,99 @@
-use super::code::{Instructions, concat_instructions};
-use super::object::Object;
 use super::ast::Program;
+use super::code::{concat_instructions, Instructions};
+use super::object::Object;
 
 pub struct Compiler {
-  instructions: Instructions,
-  constants: Vec<Object>
+    instructions: Instructions,
+    constants: Vec<Object>,
 }
 
 impl Compiler {
-  pub fn new() -> Compiler {
-    Compiler {
-      instructions: Instructions(vec![]),
-      constants: vec![]
+    pub fn new() -> Compiler {
+        Compiler {
+            instructions: Instructions(vec![]),
+            constants: vec![],
+        }
     }
-  }
 
-  pub fn compile(&mut self, program: Program) -> Result<(), String> {
-    Ok(())
-  }
-
-  pub fn bytecode(self) -> ByteCode {
-    ByteCode {
-      instructions: self.instructions,
-      constants: self.constants
+    pub fn compile(&mut self, program: Program) -> Result<(), String> {
+        Ok(())
     }
-  }
+
+    pub fn bytecode(self) -> ByteCode {
+        ByteCode {
+            instructions: self.instructions,
+            constants: self.constants,
+        }
+    }
 }
 
 pub struct ByteCode {
-  instructions: Instructions,
-  constants: Vec<Object>
+    instructions: Instructions,
+    constants: Vec<Object>,
 }
-
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-  use super::super::code::{OpcodeType,make};
-  use super::super::ast::Program;
-  use super::super::lexer::Lexer;
-  use super::super::parser::Parser;
+    use super::super::ast::Program;
+    use super::super::code::{make, OpcodeType};
+    use super::super::lexer::Lexer;
+    use super::super::parser::Parser;
+    use super::*;
 
-  #[test]
-  fn test_integer_arithmetic() {
-      let tests = vec![
-        (
-          "1 + 2".to_string(),
-          vec![1, 2],
-          vec![
-            make(OpcodeType::OpConstant.opcode(), 0),
-            make(OpcodeType::OpConstant.opcode(), 1),
-          ]
-        )
-      ];
+    #[test]
+    fn test_integer_arithmetic() {
+        let tests = vec![(
+            "1 + 2".to_string(),
+            vec![1, 2],
+            vec![
+                make(OpcodeType::OpConstant.opcode(), 0),
+                make(OpcodeType::OpConstant.opcode(), 1),
+            ],
+        )];
 
-      run_compile_tests(tests);
-  }
-
-  fn run_compile_tests(tests: Vec<(String, Vec<i64>, Vec<Instructions>)>) {
-    for (input, expected_constants, expected_instructions) in tests {
-      let program = parse(input);
-
-      let mut compiler = Compiler::new();
-      if let Err(err) = compiler.compile(program) {
-        assert!(false, "compile error. {}", err)
-      }
-
-      let bytecode = compiler.bytecode();
-
-      test_instructions(expected_instructions, bytecode.instructions);
-      test_constants(expected_constants, bytecode.constants);
+        run_compile_tests(tests);
     }
-  }
 
-  fn parse(input: String) -> Program {
-    let l = Lexer::new(&input);
-    let mut p = Parser::new(l);
-    p.parse_program()
-  }
+    fn run_compile_tests(tests: Vec<(String, Vec<i64>, Vec<Instructions>)>) {
+        for (input, expected_constants, expected_instructions) in tests {
+            let program = parse(input);
 
-  fn test_instructions(expected: Vec<Instructions>, actual: Instructions) {
-    let concated = concat_instructions(expected);
-    assert_eq!(concated, actual);
-  }
+            let mut compiler = Compiler::new();
+            if let Err(err) = compiler.compile(program) {
+                assert!(false, "compile error. {}", err)
+            }
 
-  fn test_constants(expected: Vec<i64>, actual: Vec<Object>) {
-    assert_eq!(expected.len(), actual.len());
+            let bytecode = compiler.bytecode();
 
-    for (constant, object) in expected.iter().zip(actual.iter()) {
-      test_integer_object(constant, object)
+            test_instructions(expected_instructions, bytecode.instructions);
+            test_constants(expected_constants, bytecode.constants);
+        }
     }
-  }
 
-  fn test_integer_object(expected: &i64, actual: &Object) {
-    if let Object::Integer(integer) = actual {
-      assert_eq!(expected, integer);
-    } else {
-      assert!(false, "object is not Integer. {}", actual)
+    fn parse(input: String) -> Program {
+        let l = Lexer::new(&input);
+        let mut p = Parser::new(l);
+        p.parse_program()
     }
-  }
+
+    fn test_instructions(expected: Vec<Instructions>, actual: Instructions) {
+        let concated = concat_instructions(expected);
+        assert_eq!(concated, actual);
+    }
+
+    fn test_constants(expected: Vec<i64>, actual: Vec<Object>) {
+        assert_eq!(expected.len(), actual.len());
+
+        for (constant, object) in expected.iter().zip(actual.iter()) {
+            test_integer_object(constant, object)
+        }
+    }
+
+    fn test_integer_object(expected: &i64, actual: &Object) {
+        if let Object::Integer(integer) = actual {
+            assert_eq!(expected, integer);
+        } else {
+            assert!(false, "object is not Integer. {}", actual)
+        }
+    }
 }
